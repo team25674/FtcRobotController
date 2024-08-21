799ab3ac34b89d5386f17e3ee4ab202c4a0643aa
Billy Wilson <billywilson220@gmail.com>
2024-08-20 21:56:30 -0600

    formatting. add comments. remove unused.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tutorials/TutorialSkidSteerWithMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tutorials/TutorialSkidSteerWithMode.java
@@ -5,15 +5,15 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp(name="SkidSteer_DriveMode")
-public class  TutorialSkidSteerWithMode extends OpMode {
+@TeleOp(name = "SkidSteer_DriveMode")
+public class TutorialSkidSteerWithMode extends OpMode {
 
+    // motors
     DcMotor left_motor;
     DcMotor right_motor;
 
-    // drive mode
-    boolean slow_mode_enabled; // option 1: use boolean for 2 states
-    DriveMode drive_mode; // option 2: use an enum to store many states
+    // states
+    DriveMode drive_mode;
     boolean previous_a;
 
     @Override
@@ -26,51 +26,47 @@ public class  TutorialSkidSteerWithMode extends OpMode {
         left_motor.setDirection(DcMotorSimple.Direction.FORWARD);
         right_motor.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        // initialize states
-        slow_mode_enabled = false;
+        // initialize state
         drive_mode = DriveMode.NORMAL;
     }
 
     @Override
     public void loop() {
-        // read inputs
-
-        if (previous_a && !gamepad1.a){
+        // toggle drive mode if button 'a' is released
+        if (previous_a && !gamepad1.a) {
             if (drive_mode == DriveMode.SLOW) {
                 drive_mode = DriveMode.NORMAL;
-            }else {
+            } else {
                 drive_mode = DriveMode.SLOW;
             }
         }
 
-
+        // read inputs
         double left_power_input = gamepad1.left_stick_y;
         double right_power_input = gamepad1.right_stick_y;
+
+        // update output power
         double left_power = left_power_input;
         double right_power = right_power_input;
 
-        if (drive_mode == DriveMode.SLOW){
-            left_power = left_power/2;
-            right_power = right_power/2;
-
+        if (drive_mode == DriveMode.SLOW) {
+            left_power = left_power / 2;
+            right_power = right_power / 2;
         }
+
 
         // update motors
         left_motor.setPower(left_power);
         right_motor.setPower(right_power);
 
-
-
-
         // update telemetry
         telemetry.addData("left_power", left_power);
         telemetry.addData("right_power", right_power);
-        telemetry.addData("mode",drive_mode);
+        telemetry.addData("mode", drive_mode);
         telemetry.update();
 
+        // remember current button state for next loop
         previous_a = gamepad1.a;
-
-
     }
 }
 
