6e57f6e5255f31180828886a260acb9e53f5feec
logan-tm <logan.michalicek@gmail.com>
2024-10-01 19:49:44 -0600

    Update TissueBoxExample.java
    Oops! This should've been 3 all along. Caught in pull request

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tutorials/TissueBoxExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tutorials/TissueBoxExample.java
@@ -22,6 +22,6 @@ public class TissueBoxExample {
     }
 
     private void decrementTissues() {
-        currentTissueCount = currentTissueCount - 2;
+        currentTissueCount = currentTissueCount - 3;
     }
 }
