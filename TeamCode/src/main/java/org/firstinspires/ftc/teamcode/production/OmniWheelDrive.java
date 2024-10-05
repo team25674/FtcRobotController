4c4ee74712b34a93a6550f8563fd86ad7c66747d
team25674 <team25674@broomfieldstem.org>
2024-10-05 13:41:44 -0600

    Update OmniWheelDrive.java
    Removed "Basic" from the Teleop Linear OpMode.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/production/OmniWheelDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/production/OmniWheelDrive.java
@@ -63,7 +63,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Basic: Omni Linear OpMode", group="Linear OpMode")
+@TeleOp(name="Omni Linear OpMode", group="Linear OpMode")
 @Disabled
 public class OmniWheelDrive extends LinearOpMode {
 
