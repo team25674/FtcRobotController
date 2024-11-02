518e896c8fd03d9108a73235d346b0bf867fdcc0
team25674 <team25674@broomfieldstem.org>
2024-11-02 14:10:37 -0600

    Implemented linearSlide class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/mechanisms/LinearSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/mechanisms/LinearSlide.java
@@ -1,4 +1,37 @@
 package org.firstinspires.ftc.teamcode.lib.mechanisms;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class LinearSlide {
+    static final double COUNTS_PER_INCH = 112.8181;
+    public static int POS_LOWER_BASKET_INCHES = 5;
+    public static int POS_UPPER_BASKET_INCHES = 10;
+    public DcMotor motor;
+    int max;
+
+    public LinearSlide(DcMotor motor, int max) {
+        this.motor = motor;
+        this.max = max;
+    }
+
+    public void goToPosition(int positionInches) {
+        // Determine new target position, and pass to motor controller
+        int newTarget = (int) (positionInches * COUNTS_PER_INCH);
+        motor.setTargetPosition(newTarget);
+
+        // Turn On RUN_TO_POSITION
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // Start motion
+        motor.setPower(1);
+    }
+
+    public void goToZero() {
+        goToPosition(0);
+    }
+
+    public void extend(double speed) {
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor.setPower(speed);
+    }
 }
