27c8866157d3c20de25f7b27e360add6508fd84f
team25674 <team25674@broomfieldstem.org>
2024-12-03 18:47:26 -0700

    Claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/mechanisms/ClawAttachment.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/mechanisms/ClawAttachment.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode.lib.mechanisms;
 
 import com.qualcomm.robotcore.hardware.Servo;
 
-public class Spy {
+public class ClawAttachment {
     private final double UP_POSITION = 1;
     private final double DOWN_POSTION = .43;
     private final double REJECT_POSITION_WHEEL1 = .1;
@@ -14,15 +14,14 @@ public class Spy {
     private Servo wheel2;
     private Servo upAndDown;
 
-    public Spy (Servo wheel1, Servo wheel2, Servo upAndDown){
+    public ClawAttachment(Servo wheel1, Servo wheel2, Servo upAndDown){
         this.wheel1 = wheel1;
         this.wheel2 = wheel2;
         this.upAndDown = upAndDown;
     }
     public void intake() {
         wheel1.setPosition(INTAKE_POSITION_WHEEL1);
-        wheel2.setPosition(INTAKE_POSITION_WHEEL2);
-     // change to set a direction (continuous)
+
     }
     public void reject() {
         wheel1.setPosition(REJECT_POSITION_WHEEL1);
